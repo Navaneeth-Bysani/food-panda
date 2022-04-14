93b46582bd3b1a143fb40a4e77b3566f166cd155
Hey You! <ramkaipoke333@gmail.com>
2022-04-14 17:25:01 +0530

    Added login page

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SignIn from './pages/login/SignIn';
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn/>
+    </div>
+  );
+}
+
+export default App;
