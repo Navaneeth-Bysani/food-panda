6aa7df74406a45420c7e670a996b643acfd00e0b
pbr45 <103638838+pbr45@users.noreply.github.com>
2022-04-14 16:42:56 +0530

    db started
    abcd

--- server/app.js
+++ server/app.js
@@ -2,4 +2,24 @@ const express = require('express');
 
 const app = express();
 
+app.get('/login', (req,res) => {
+    
+    const userDetails = {
+        email : 'navaneeth@gmail.com',
+        password : 'navaneeth'
+    };
+
+    // //find if user email is present in db
+    // let email = select email from users where email = userDetails.email;
+    // if(email != NULL) {
+    //     //check password is correct or not
+    //     select pwd from users where email = userDetails.email;
+    //     if(pwd == userDetails.password) {
+    //         //user exists
+    //     } else {
+    //         //password incorrect
+    //     }
+    // }
+
+});
 module.exports = app;
---
+++ server/db/db.sql
@@ -0,0 +1,1 @@
+create table user(username varchar(30),phone number,password varchar(10),email varchar(10));
