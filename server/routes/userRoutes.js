0a00cd6db17043ad4a60042dd1e9b06f13d14007
Navaneeth Bysani <61292292+Navaneeth-Bysani@users.noreply.github.com>
2022-04-19 19:49:23 +0530

    edited user table

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const userController = require('./../controllers/userController');
 
 router.get('/', userController.getAllUsers);
-
+router.post('/order', userController.placeOrder);
 
 module.exports = router;
